d26be4d41ad48c44fcda7e3f20d2919dac418e38
CotterellAnna <cotterellanna@gmail.com>
2021-09-08 21:54:32 +0100

    profile nav

--- nav.js
+++ nav.js
@@ -6,10 +6,25 @@ function myFunction(){
     if(navMenu.style.display=="none"){
         navMenu.style.display="flex";
         navMenu.style.animationName='slideIn';
-        body.style.overflow='hidden'
+        //body.style.overflow='hidden'
     }else{
         navMenu.style.display="none";
-        body.style.overflow='scroll'
+        //body.style.overflow='scroll'
     }
     
+}
+
+let profile_icon = document.getElementById('profile_icon');
+let profile_dropdown = document.getElementById('profile_dropdown');
+let user = document.querySelector('.user');
+console.log(user)
+
+user.addEventListener('click', profile)
+
+function profile(){
+    if(profile_dropdown.style.display == "none"){
+        profile_dropdown.style.display = "flex";
+    }else{
+        profile_dropdown.style.display='none';
+    }
 }
--- user.js
+++ user.js
@@ -1,3 +1,22 @@
-let username = document.getElementById('user');
-let user_data = JSON.parse(localStorage.getItem('user'));
-username.innerText = user_data.name;
+let dropdown_content = document.getElementById('dropdown-content');
+
+
+function dropdown(){
+    if(dropdown_content.style.display == 'none'){
+        dropdown_content.style.display = 'block'
+    }else{
+        dropdown_content.style.display = 'none'
+    }
+}
+
+let user_ = JSON.parse(localStorage.getItem('user'));
+let user_login = JSON.parse(sessionStorage.getItem('user'));
+let logout = document.getElementById('logout');
+
+logout.addEventListener('click', logout_)
+function logout_(e){
+    e.preventDefault();
+    sessionStorage.clear();
+    let current = window.location.href
+    window.location.href = window.location.href.replace(current, 'login.html');
+}
