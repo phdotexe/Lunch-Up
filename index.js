ca5fcf22f86711ef794d2ad7ad36e3fd0fbdc3c1
Dabsia <dabojohnson98@gmail.com>
2021-09-20 09:41:27 +0100

    More updates on the location

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ Form.addEventListener('submit', ShowLocation)
 user_location.focus()
 function ShowLocation(e){
     e.preventDefault()
-    // let current_href = window.location.href;
+    let current_href = window.location.href;
     if (user_location.value.length > 0 ){
         localStorage.setItem('user_location', user_location.value)
         window.location.href = window.location.href.replace(current_href, 'signup.html');
     }
-    // let localServer = '127.0.0.1:5502';
+    let localServer = '127.0.0.1:5502';
     // let current_href = window.location.href;
     // let current_pathname = window.location.pathname;
 
