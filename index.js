5f1481162ab4e90b4a7fc35e73777f65c0752a2f
Dabsia <dabojohnson98@gmail.com>
2021-09-20 09:32:42 +0100

    User location

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ user_location.focus()
 function ShowLocation(e){
     e.preventDefault()
     // let current_href = window.location.href;
-    // if (user_location.value.length > 0 ){
-    //     localStorage.setItem('user_location', user_location.value)
-    //     window.location.href = window.location.href.replace(current_href, 'signup.html');
-    // }
-    let localServer = '127.0.0.1:5502';
-    let current_href = window.location.href;
-    let current_pathname = window.location.pathname;
-
-    if(window.location.href.includes(localServer) && user_location.value.length > 0 ) {
+    if (user_location.value.length > 0 ){
         localStorage.setItem('user_location', user_location.value)
-        window.location.href = window.location.href.replace(current_pathname, '/signup.html');
-    }else{
-        window.location.href = window.location.href.replace(current_pathname, `${current_pathname}signup.html`);
-    }    
+        window.location.href = window.location.href.replace(current_href, 'signup.html');
+    }
+    // let localServer = '127.0.0.1:5502';
+    // let current_href = window.location.href;
+    // let current_pathname = window.location.pathname;
+
+    // if(window.location.href.includes(localServer) && user_location.value.length > 0 ) {
+    //     localStorage.setItem('user_location', user_location.value)
+    //     window.location.href = window.location.href.replace(current_pathname, '/signup.html');
+    // }else{
+    //     window.location.href = window.location.href.replace(current_pathname, `${current_pathname}signup.html`);
+    // }    
 }
